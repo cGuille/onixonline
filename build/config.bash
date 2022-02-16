3fb5d85a44d09ad7b4f52713996648e70f5aedee
Guillaume Charmetant <cguille.dev@gmail.com>
2022-02-16 12:55:08 +0100

    Add build process

---
+++ build/config.bash
@@ -0,0 +1,2 @@
+# From https://www.editeur.org/93/Release-3.0-Downloads (HTML in ZIP link)
+SPEC_URL='https://www.editeur.org/files/ONIX%203/ONIX_for_Books_Release3-0_html_Best_Practice+codes_Issue_56_v1-8-3.zip'
